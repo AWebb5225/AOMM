67d54e2206b45e2707af396e50c84f71eaf61740
AWebb5225 <alexander.webber5225@gmail.com>
2024-10-01 20:34:57 -0700

    partially added getArgs and renamed processIDs to allProcessID

--- AOMM/main.m
+++ AOMM/main.m
@@ -1,10 +1,10 @@
-//
+// source: https://github.com/objective-see/TAOMM/blob/main/Code/Vol%20II/CH%201/enumerateProcesses/enumerateProcesses/main.m
 //  main.m
 //  AOMM
 //
 //  Created by Alexander on 10/1/24.
 //
-//TODO: print out the audit token, pg. 6 Paths and Names
+//TODO: fix getArgs, pg. 13
 
 #import <Foundation/Foundation.h>
 #import <sys/sysctl.h>
@@ -13,19 +13,19 @@
 
 // Function declarations
 NSData* getAuditToken(pid_t pid);
-NSMutableArray* getProcessIDs(void);
+NSMutableArray* getAllProcessID(void);
 int getPath(pid_t pid);
 NSString* getAppName(pid_t pid);
 
 int main(int argc, const char *argv[]) {
     NSMutableArray* pids = nil;
-    pids = getProcessIDs();
+    pids = getAllProcessID();
     
     return 0;
 }
 
 //List all running processes
-NSMutableArray* getProcessIDs(void) {
+NSMutableArray* getAllProcessID(void) {
     int32_t processesCount = 0;
     
     //Dynamically retrieve max # of running processes
@@ -38,16 +38,16 @@ NSMutableArray* getProcessIDs(void) {
     
     printf("Found %d running processes\n", processesCount);
     
-    NSMutableArray* processIDs = [[NSMutableArray alloc] init]; //have to make pointer array to store pid objects
+    NSMutableArray* allProccessID = [[NSMutableArray alloc] init]; //have to make pointer array to store pid objects
     for(int i = 0; i < processesCount; i++){
         //integer objects being added to processID object array
-        [processIDs addObject:[NSNumber numberWithInt:pids[i]]]; //fill array with pids
+        [allProccessID addObject:[NSNumber numberWithInt:pids[i]]]; //fill array with pids
         printf("%d\t\n", pids[i]);
        // printf("%c", (char)getAppName(pids[i])) ### should get app name but returns numbers
     }
     
     free(pids); //free memory
-    return processIDs;
+    return allProccessID;
 }
 
 //Obtain audit token for a process
@@ -79,4 +79,40 @@ NSString* getAppName(pid_t pid) {
         name = bundle.infoDictionary[@"CFBundleName"];
     }
     return name;
+}
+
+// get cmdl args and save them in 'arguments' ivar
+NSMutableArray* getArgs(pid_t pid) {
+    int mib[3] = {0}; // initialize Management Information Base array
+    int systemMaxArgs = 0;
+    int numArgs = 0;
+    NSString* argument = nil;
+    char* parser = NULL;
+    size_t size = sizeof(systemMaxArgs);
+    
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_ARGMAX; //max size of arguments
+    sysctl(mib, 2, &systemMaxArgs, &size, NULL, 0);
+    
+    char* args = malloc(systemMaxArgs); // Create buffer for arguments
+    
+    size = (size_t)systemMaxArgs;
+        
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROCARGS2; //KERN_PROCARGS2 == Num args (int argc), Process path, argv[0]/[1]/[...]
+    mib[2] = pid;
+    sysctl(mib, 3, args, &size, NULL, 0);
+    
+    NSMutableArray* extractedArgs = [NSMutableArray array];
+    memcpy(&numArgs, args, sizeof(numArgs));
+    parser = argument + sizeof(numArgs);
+    
+    while (NULL != *++parser);
+    while (NULL == *++parser);
+    while (extractedArgs.count < numArgs) {
+        [extractedArgs addObject:[NSString stringWithUTF8String:parser]];
+        parser += strlen(parser) + 1;
+    }
+    
+    return 0;
 }
