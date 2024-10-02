da75f7cbea24a644d8368edf0fcf9382b5f05397
AWebb5225 <alexander.webber5225@gmail.com>
2024-10-01 17:20:22 -0700

    reordered methods and added getAppPath which is WiP

--- AOMM/main.m
+++ AOMM/main.m
@@ -4,16 +4,18 @@
 //
 //  Created by Alexander on 10/1/24.
 //
-
 //TODO: print out the audit token, pg. 6 Paths and Names
 
 #import <Foundation/Foundation.h>
 #import <sys/sysctl.h>
 #import <libproc.h>
+#import <AppKit/AppKit.h>
 
 // Function declarations
 NSData* getAuditToken(pid_t pid);
 NSMutableArray* getProcessIDs(void);
+int getPath(pid_t pid);
+NSString* getAppName(pid_t pid);
 
 int main(int argc, const char *argv[]) {
     NSMutableArray* pids = nil;
@@ -22,20 +24,7 @@ int main(int argc, const char *argv[]) {
     return 0;
 }
 
-//Obtain audit token for a process
-NSData* getAuditToken(pid_t pid) {
-    NSData* auditToken = nil;
-    task_name_t task = {0}; // initialize auditToken, tasks and tokens
-    audit_token_t token = {0};
-    mach_msg_type_number_t info_size = TASK_AUDIT_TOKEN_COUNT;
-    
-    task_name_for_pid(mach_task_self(), pid, &task); //required to hold audit token
-    task_info(task, TASK_AUDIT_TOKEN, (integer_t *) &token, &info_size); //populate task with audit token
-    auditToken = [NSData dataWithBytes:&token length:sizeof(audit_token_t)]; //make token an object
-    
-    return auditToken;
-}
-
+//List all running processes
 NSMutableArray* getProcessIDs(void) {
     int32_t processesCount = 0;
     
@@ -53,9 +42,41 @@ NSMutableArray* getProcessIDs(void) {
     for(int i = 0; i < processesCount; i++){
         //integer objects being added to processID object array
         [processIDs addObject:[NSNumber numberWithInt:pids[i]]]; //fill array with pids
-        printf("%d\n", pids[i]);
+        printf("%d\t\n", pids[i]);
+       // printf("%c", (char)getAppName(pids[i])) ### should get app name but returns numbers
     }
     
     free(pids); //free memory
     return processIDs;
+}
+
+//Obtain audit token for a process
+NSData* getAuditToken(pid_t pid) {
+    NSData* auditToken = nil;
+    task_name_t task = {0}; // initialize auditToken, tasks and tokens
+    audit_token_t token = {0};
+    mach_msg_type_number_t info_size = TASK_AUDIT_TOKEN_COUNT;
+    
+    task_name_for_pid(mach_task_self(), pid, &task); //required to hold audit token
+    task_info(task, TASK_AUDIT_TOKEN, (integer_t *) &token, &info_size); //populate task with audit token
+    auditToken = [NSData dataWithBytes:&token length:sizeof(audit_token_t)]; //make token an object
+    
+    return auditToken;
+}
+
+//find the file path for a process
+int getPath(pid_t pid) {
+    char path[PROC_PIDPATHINFO_MAXSIZE] = {0};
+    return proc_pidpath(pid, path, PROC_PIDPATHINFO_MAXSIZE);
+}
+
+//Extract application name from running process
+NSString* getAppName(pid_t pid) {
+    NSString* name = nil;
+    NSRunningApplication* app = [NSRunningApplication runningApplicationWithProcessIdentifier:pid];
+    if (nil != app) {
+        NSBundle* bundle = [NSBundle bundleWithURL:app.bundleURL];
+        name = bundle.infoDictionary[@"CFBundleName"];
+    }
+    return name;
 }
